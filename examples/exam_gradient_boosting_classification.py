829a6e710fe73dcdd26870282506235292f946dd
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-10 10:34:19 +0700

    Add examples for Gradient Boosting models

---
+++ examples/exam_gradient_boosting_classification.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 10:10, 10/05/2025 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from metasklearn import MetaSearchCV, IntegerVar, StringVar, FloatVar
+
+# Load dataset
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define param bounds
+
+# param_grid = {            ==> This is for GridSearchCV, show you how to convert to our MetaSearchCV
+param_grid = {
+    'n_estimators': [50, 75, 100],                 # Number of boosting stages (trees)
+    'learning_rate': [0.01, 0.05, 0.1, 0.2],         # Shrinks the contribution of each tree
+    'max_depth': [3, 5, 7],                          # Maximum depth of individual trees
+    'min_samples_split': [2, 5, 10],                 # Minimum number of samples to split a node
+    'min_samples_leaf': [1, 2, 4],                   # Minimum samples required at a leaf node
+    'max_features': ['auto', 'sqrt', 'log2'],        # Number of features to consider when looking for the best split
+    'subsample': [1.0, 0.8, 0.6],                    # Fraction of samples to be used for fitting the individual base learners
+    'loss': ['log_loss'],             # Classification loss functions
+    'ccp_alpha': [0.0, 0.01, 0.1],                   # Complexity parameter for Minimal Cost-Complexity Pruning
+}
+
+param_bounds = [
+    IntegerVar(lb=50, ub=200, name="n_estimators"),  # Số lượng cây trong mô hình
+    FloatVar(lb=0.01, ub=0.2, name="learning_rate"),  # Tốc độ học
+    IntegerVar(lb=2, ub=5, name="max_depth"),  # Độ sâu tối đa của cây
+    IntegerVar(lb=2, ub=10, name="min_samples_split"),  # Số lượng mẫu tối thiểu để một node được chia
+    IntegerVar(lb=1, ub=5, name="min_samples_leaf"),  # Số lượng mẫu tối thiểu tại một node lá
+    StringVar(valid_sets=("sqrt", "log2"), name="max_features"),  # Số đặc trưng tối đa khi tìm split tốt nhất
+    FloatVar(lb=0.5, ub=1.0, name="subsample"),  # Tỉ lệ mẫu được sử dụng để fit base learners
+    StringVar(valid_sets=(("log_loss",)), name="loss"),  # (classification)
+    FloatVar(lb=0.0, ub=0.1, name="ccp_alpha"),  # Tham số điều chỉnh cho pruning
+]
+
+# Initialize and fit MetaSearchCV
+searcher = MetaSearchCV(
+    estimator=GradientBoostingClassifier(random_state=42),
+    param_bounds=param_bounds,
+    task_type="classification",
+    optim="BaseGA",
+    optim_params={"epoch": 20, "pop_size": 30, "name": "GA"},
+    cv=3,
+    scoring="AS",  # or any custom scoring like "F1_macro"
+    seed=42,
+    n_jobs=2,
+    verbose=True
+)
+
+searcher.fit(X_train, y_train)
+print("Best parameters:", searcher.best_params)
+print("Best model: ", searcher.best_estimator)
+print("Best score during searching: ", searcher.best_score)
+
+# Make prediction after re-fit
+y_pred = searcher.predict(X_test)
+print("Test Accuracy:", searcher.score(X_test, y_test))
+print("Test Score: ", searcher.scores(X_test, y_test, list_metrics=("AS", "RS", "PS", "F1S")))
---
+++ examples/exam_gradient_boosting_regression.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 10:05, 10/05/2025 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.datasets import load_diabetes
+from metasklearn import MetaSearchCV, IntegerVar, StringVar, FloatVar, Data
+
+## Load data object
+X, y = load_diabetes(return_X_y=True)
+data = Data(X, y)
+
+## Split train and test
+data.split_train_test(test_size=0.2, random_state=42, inplace=True)
+print(data.X_train.shape, data.X_test.shape)
+
+## Scaling dataset
+data.X_train, scaler_X = data.scale(data.X_train, scaling_methods=("standard", "minmax"))
+data.X_test = scaler_X.transform(data.X_test)
+
+data.y_train, scaler_y = data.scale(data.y_train, scaling_methods=("standard", "minmax"))
+data.y_train = data.y_train.ravel()
+data.y_test = scaler_y.transform(data.y_test.reshape(-1, 1)).ravel()
+
+# Define param bounds
+
+# param_grid = {            ==> This is for GridSearchCV, show you how to convert to our MetaSearchCV
+#     'n_estimators': [50, 75, 100],                 # Number of boosting stages (trees)
+#     'learning_rate': [0.01, 0.05, 0.1, 0.2],         # Shrinks the contribution of each tree
+#     'max_depth': [3, 5, 7],                          # Maximum depth of individual trees
+#     'min_samples_split': [2, 5, 10],                 # Minimum number of samples to split a node
+#     'min_samples_leaf': [1, 2, 4],                   # Minimum samples required at a leaf node
+#     'max_features': ['auto', 'sqrt', 'log2'],        # Number of features to consider when looking for the best split
+#     'subsample': [1.0, 0.8, 0.6],                    # Fraction of samples to be used for fitting the individual base learners
+#     'loss': ['squared_error', 'absolute_error', 'huber'],  # Regression loss functions
+#     'ccp_alpha': [0.0, 0.01, 0.1],                   # Complexity parameter for Minimal Cost-Complexity Pruning
+# }
+
+param_bounds = [
+    IntegerVar(lb=50, ub=200, name="n_estimators"),  # Số lượng cây trong mô hình
+    FloatVar(lb=0.01, ub=0.2, name="learning_rate"),  # Tốc độ học
+    IntegerVar(lb=2, ub=5, name="max_depth"),  # Độ sâu tối đa của cây
+    IntegerVar(lb=2, ub=10, name="min_samples_split"),  # Số lượng mẫu tối thiểu để một node được chia
+    IntegerVar(lb=1, ub=5, name="min_samples_leaf"),  # Số lượng mẫu tối thiểu tại một node lá
+    StringVar(valid_sets=("sqrt", "log2"), name="max_features"),  # Số đặc trưng tối đa khi tìm split tốt nhất
+    FloatVar(lb=0.5, ub=1.0, name="subsample"),  # Tỉ lệ mẫu được sử dụng để fit base learners
+    StringVar(valid_sets=("squared_error", "absolute_error", 'huber'), name="loss"),
+    FloatVar(lb=0.0, ub=0.1, name="ccp_alpha"),  # Tham số điều chỉnh cho pruning
+]
+
+# Initialize and fit MetaSearchCV
+searcher = MetaSearchCV(
+    estimator=GradientBoostingRegressor(random_state=42),
+    param_bounds=param_bounds,
+    task_type="regression",
+    optim="BaseGA",
+    optim_params={"epoch": 20, "pop_size": 30, "name": "GA"},
+    cv=3,
+    scoring="MSE",  # or any custom scoring like "F1_macro"
+    seed=42,
+    n_jobs=2,
+    verbose=True
+)
+
+searcher.fit(data.X_train, data.y_train)
+print("Best parameters:", searcher.best_params)
+print("Best model: ", searcher.best_estimator)
+print("Best score during searching: ", searcher.best_score)
+
+# Make prediction after re-fit
+y_pred = searcher.predict(data.X_test)
+print("Test R2:", searcher.score(data.X_test, data.y_test))
+print("Test Score: ", searcher.scores(data.X_test, data.y_test, list_metrics=("RMSE", "R", "KGE", "NNSE")))
